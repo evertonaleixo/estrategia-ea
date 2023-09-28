b4568d855ca34f4e4abba8cc5304cdd7110feb47
Everton Aleixo <everton.a@sidia.com>
2023-09-28 00:36:25 -0400

    text a theory

--- api/models.py
+++ api/models.py
@@ -1,11 +1,19 @@
-from pydantic import BaseModel  # Importe o BaseModel
-from typing import Optional
+from fastapi import FastAPI
 
-# Modelo para receber os dados do registro
-class User(BaseModel):
-    name: str
-    email: str
-    password: str
-    birthdate: Optional[str] = None
-    state: Optional[str] = None
-    city: Optional[str] = None
+# from pydantic import BaseModel  # Importe o BaseModel
+# from typing import Optional
+
+# # Modelo para receber os dados do registro
+# class User(BaseModel):
+#     name: str
+#     email: str
+#     password: str
+#     birthdate: Optional[str] = None
+#     state: Optional[str] = None
+#     city: Optional[str] = None
+
+app = FastAPI()
+
+@app.get("/api/python")
+def hello_world():
+    return {"message": "Hello World"}
